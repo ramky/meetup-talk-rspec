1c2f5f1be8d562e1393af997fef06cf308d4a5d5
Ram Iyer <ramky.iyer@yahoo.com>
2015-04-12 11:13:36 -0700

    housekeeping

--- spec/spies/statement_spec.rb
+++ spec/spies/statement_spec.rb
@@ -10,11 +10,6 @@ describe Statement do
 
     header = Statement.new(customer).generate_header
 
-    # need example of clutter due to stubs
-    # all spies are stubs in rspec-mocks
-    # when you are testing side effects stubs are of no value
-    # test a command method use mock
-    # test a query method use stubs
     expect(customer).to have_received(:name)
     expect(header).to eq "Statement for #{name}"
   end
